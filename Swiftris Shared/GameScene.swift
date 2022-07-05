b9228cc448091ea318e8c1722fd98eee081aad2f
Nilton <niltoneapontes@gmail.com>
2022-07-05 00:04:09 -0300

    feat: add blocks and basic functionalities

--- Swiftris Shared/GameScene.swift
+++ Swiftris Shared/GameScene.swift
@@ -7,13 +7,38 @@
 
 import SpriteKit
 
+let BlockSize:CGFloat = 20.0
+
 let TickLengthLevelOne = TimeInterval(600)
 
+extension CGRect{
+    init(_ x:CGFloat,_ y:CGFloat,_ width:CGFloat,_ height:CGFloat) {
+        self.init(x:x,y:y,width:width,height:height)
+    }
+
+}
+extension CGSize{
+    init(_ width:CGFloat,_ height:CGFloat) {
+        self.init(width:width,height:height)
+    }
+}
+extension CGPoint{
+    init(_ x:CGFloat,_ y:CGFloat) {
+        self.init(x:x,y:y)
+    }
+}
+
 class GameScene: SKScene {
+    let gameLayer = SKNode()
+    let shapeLayer = SKNode()
+    let LayerPosition = CGPoint(x: 6, y: -6)
     
     var tick:(() -> ())?
     var tickLengthMillis = TickLengthLevelOne
     var lastTick:NSDate?
+    
+    var textureCache = Dictionary<String, SKTexture>()
+
     
     required init(coder aDecoder: NSCoder) {
         fatalError("NSCoder not supported")
@@ -30,6 +55,17 @@ class GameScene: SKScene {
         background.anchorPoint = CGPoint(x: 0, y: 1.0)
         background.scale(to: self.frame.size)
         addChild(background)
+        
+        addChild(gameLayer)
+
+                let gameBoardTexture = SKTexture(imageNamed: "gameboard")
+                let gameBoard = SKSpriteNode(texture: gameBoardTexture, size: CGSize(BlockSize * CGFloat(NumColumns), BlockSize * CGFloat(NumRows)))
+                gameBoard.anchorPoint = CGPoint(x:0, y:1.0)
+                gameBoard.position = LayerPosition
+
+                shapeLayer.position = LayerPosition
+                shapeLayer.addChild(gameBoard)
+                gameLayer.addChild(shapeLayer)
     }
     
     override func update(_ currentTime: TimeInterval) {
@@ -52,5 +88,63 @@ class GameScene: SKScene {
 
     func stopTicking() {
         lastTick = nil
+    }
+    
+    func pointForColumn(column: Int, row: Int) -> CGPoint {
+        let x = LayerPosition.x + (CGFloat(column) * BlockSize) + (BlockSize / 2)
+        let y = LayerPosition.y - ((CGFloat(row) * BlockSize) + (BlockSize / 2))
+        return CGPoint(x, y)
+    }
+
+    func addPreviewShapeToScene(shape:Shape, completion:@escaping () -> ()) {
+        for block in shape.blocks {
+// #10
+            var texture = textureCache[block.spriteName]
+            if texture == nil {
+                texture = SKTexture(imageNamed: block.spriteName)
+                textureCache[block.spriteName] = texture
+            }
+            let sprite = SKSpriteNode(texture: texture)
+// #11
+            sprite.position = pointForColumn(column: block.column, row:block.row - 2)
+            shapeLayer.addChild(sprite)
+            block.sprite = sprite
+
+            // Animation
+            sprite.alpha = 0
+// #12
+            let moveAction = SKAction.moveTo(pointForColumn(column: block.column, row: block.row), duration: NSTimeInterval(0.2))
+            moveAction.timingMode = .EaseOut
+            let fadeInAction = SKAction.fadeAlphaTo(0.7, duration: 0.4)
+            fadeInAction.timingMode = .EaseOut
+            sprite.runAction(SKAction.group([moveAction, fadeInAction]))
+       }
+        runAction(SKAction.waitForDuration(0.4), completion: completion)
+    }
+
+    func movePreviewShape(shape:Shape, completion:() -> ()) {
+        for block in shape.blocks {
+            let sprite = block.sprite!
+            let moveTo = pointForColumn(column: block.column, row:block.row)
+            let moveToAction:SKAction = SKAction.moveTo(moveTo, duration: 0.2)
+            moveToAction.timingMode = .EaseOut
+            sprite.runAction(
+                SKAction.group([moveToAction, SKAction.fadeAlphaTo(1.0, duration: 0.2)]), completion: {})
+        }
+        runAction(SKAction.waitForDuration(0.2), completion: completion)
+    }
+
+    func redrawShape(shape:Shape, completion:() -> ()) {
+        for block in shape.blocks {
+            let sprite = block.sprite!
+            let moveTo = pointForColumn(column: block.column, row:block.row)
+            let moveToAction:SKAction = SKAction.moveTo(moveTo, duration: 0.05)
+            moveToAction.timingMode = .EaseOut
+            if block == shape.blocks.last {
+                sprite.runAction(moveToAction, completion: completion)
+            } else {
+                sprite.runAction(moveToAction)
+            }
+        }
     }
 }
--- Swiftris iOS/GameViewController.swift
+++ Swiftris iOS/GameViewController.swift
@@ -12,6 +12,7 @@ import GameplayKit
 class GameViewController: UIViewController {
 
     var scene: GameScene!
+    var swiftris:Swiftris!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,12 +26,32 @@ class GameViewController: UIViewController {
         scene.backgroundColor = UIColor.blue
         scene.scaleMode = .aspectFill
         
+        scene.tick = didTick
+
+        swiftris = Swiftris()
+        swiftris.beginGame()
+
+        
         // Present the scene
         skView.presentScene(scene)
+        
+        scene.addPreviewShapeToScene(shape: swiftris.nextShape!) {
+            self.swiftris.nextShape?.moveTo(column: StartingColumn, row: StartingRow)
+            self.scene.movePreviewShape(shape: self.swiftris.nextShape!) {
+                let nextShapes = self.swiftris.newShape()
+                self.scene.startTicking()
+                self.scene.addPreviewShapeToScene(shape: nextShapes.nextShape!) {}
+            }
+        }
         
     }
 
     override var prefersStatusBarHidden: Bool {
         return true
+    }
+    
+    func didTick() {
+        swiftris.fallingShape?.lowerShapeByOneRow()
+        scene.redrawShape(shape: swiftris.fallingShape!, completion: {})
     }
 }
